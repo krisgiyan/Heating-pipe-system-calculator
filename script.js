340091ea3993b33ff26bcc4b763282c88f4af0c5
Kristiyan Ivanov <kris.ivanov95@outlook.com>
2020-11-28 21:33:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,319 @@
+function Calc() {
+    let width = document.getElementById("width").value;
+    let temp = document.getElementById("temp").value;
+    let diameter = document.getElementById("diameter").value;
+
+
+    if (diameter == 15) {
+        if (width == 10) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 7
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 11
+            }
+            
+        } else if(width == 20) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 5
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 7
+            }
+
+        }else if(width == 30) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 4
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 6
+            }
+        }
+        
+    }
+
+
+    if (diameter == 20) {
+        if (width == 10) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 9
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 14
+            }
+            
+        } else if(width == 20) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 6
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 9
+            }
+
+        }else if(width == 30) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 5
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 7
+            }
+        }
+        
+    }
+
+
+
+    if (diameter == 25) {
+        if (width == 10) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 11
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 16
+            }
+            
+        } else if(width == 20) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 7
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 10
+            }
+
+        }else if(width == 30) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 5
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 8
+            }
+        }
+        
+    }
+
+
+
+
+    if (diameter == 32) {
+        if (width == 10) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 13
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 19
+            }
+            
+        } else if(width == 20) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 8
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 12
+            }
+
+        }else if(width == 30) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 6
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 9
+            }
+        }
+        
+    }
+
+
+
+    if (diameter == 40) {
+        if (width == 10) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 15
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 23
+            }
+            
+        } else if(width == 20) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 9
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 14
+            }
+
+        }else if(width == 30) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 7
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 10
+            }
+        }
+        
+    }
+
+
+
+
+    if (diameter == 50) {
+        if (width == 10) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 19
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 28
+            }
+            
+        } else if(width == 20) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 11
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 16
+            }
+
+        }else if(width == 30) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 8
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 12
+            }
+        }
+        
+    }
+
+
+
+
+
+    if (diameter == 65) {
+        if (width == 10) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 23
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 35
+            }
+            
+        } else if(width == 20) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 13
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 20
+            }
+
+        }else if(width == 30) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 10
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 14
+            }
+        }
+        
+    }
+
+
+
+
+
+    if (diameter == 80) {
+        if (width == 10) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 28
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 42
+            }
+            
+        } else if(width == 20) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 15
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 23
+            }
+
+        }else if(width == 30) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 11
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 17
+            }
+        }
+        
+    }
+
+
+
+
+
+
+
+    if (diameter == 100) {
+        if (width == 10) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 34
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 52
+            }
+            
+        } else if(width == 20) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 19
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 28
+            }
+
+        }else if(width == 30) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 13
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 20
+            }
+        }
+        
+    }
+
+
+
+
+
+    if (diameter == 150) {
+        if (width == 10) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 50
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 75
+            }
+            
+        } else if(width == 20) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 27
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 40
+            }
+
+        }else if(width == 30) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 19
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 28
+            }
+        }
+        
+    }
+
+
+
+
+
+    if (diameter == 200) {
+        if (width == 10) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 66
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 99
+            }
+            
+        } else if(width == 20) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 34
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 52
+            }
+
+        }else if(width == 30) {
+            if (temp == -15) {
+                document.getElementById("result").innerHTML = 24
+            }else if (temp == -25) {
+                document.getElementById("result").innerHTML = 36
+            }
+        }
+        
+    }
+
+
+
+
+
+}
